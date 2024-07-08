72d9e959205683143dea41f3a46f1cd84f019f50
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-07-08 21:00:23 +0530

    add entity

---
+++ src/main/java/com/ckproj/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.ckproj.ems_backend.repository;
+
+import com.ckproj.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
